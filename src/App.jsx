d4b934adbaaca384d0f7ae389c77fdd4a618e0cb
ritiksharmarj <theonlyritik@gmail.com>
2023-04-02 17:27:34 +0530

    Implementing FactList

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
+
 import './App.css';
 import Header from './components/Header';
 import CategoryFilter from './components/CategoryFilter';
 import FactList from './components/FactList';
+import Loader from './components/Loader';
 
 const App = () => {
+  const [currentCategory, setCurrentCategory] = useState('all');
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
       <Header />
 
-      <main className='mx-auto max-w-[1440px]'>
-        <div className='flex px-4 sm:px-6 lg:px-8 h-screen pt-24'>
-          <CategoryFilter />
-          <FactList />
+      <main className='mx-auto max-w-[1440px] overflow-hidden'>
+        <div className='grid sm:grid-cols-[250px_minmax(0,_1fr)] grid-cols-1 gap-12 px-4 sm:px-6 lg:px-8 sm:h-screen h-auto pt-28'>
+          <CategoryFilter setCurrentCategory={setCurrentCategory} />
+
+          {isLoading ? <Loader /> : <FactList />}
         </div>
       </main>
     </>
--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+const CategoryFilter = ({ setCurrentCategory }) => {
+  const CATEGORIES = [
+    { name: 'technology', color: '#3b82f6' },
+    { name: 'science', color: '#16a34a' },
+    { name: 'finance', color: '#ef4444' },
+    { name: 'society', color: '#eab308' },
+    { name: 'entertainment', color: '#db2777' },
+    { name: 'health', color: '#14b8a6' },
+    { name: 'history', color: '#f97316' },
+    { name: 'news', color: '#8b5cf6' },
+  ];
 
-const CategoryFilter = () => {
-  return <div>CategoryFilter</div>;
+  return (
+    <div>
+      <ul className='flex sm:flex-col sm:gap-4 gap-2 overflow-x-scroll'>
+        <li>
+          <button
+            className='btn-gradient w-full font-coiny leading-none uppercase sm:text-base text-sm sm:px-4 sm:pt-4 sm:pb-[13px] p-3 pb-[9px] rounded-full transition-all duration-300 sm:hover:scale-110 hover:-rotate-2 hover:scale-105 mb-4'
+            onClick={() => setCurrentCategory('all')}
+          >
+            All
+          </button>
+        </li>
+
+        {CATEGORIES.map((item) => (
+          <li key={item.name}>
+            <button
+              className='w-full font-coiny leading-none uppercase sm:text-base text-sm sm:px-4 sm:pt-4 sm:pb-[13px] p-3 pb-[9px] rounded-full transition-all duration-300 sm:hover:scale-110 hover:-rotate-2 hover:scale-105'
+              style={{ backgroundColor: item.color }}
+              onClick={() => setCurrentCategory(item.name)}
+            >
+              {item.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CategoryFilter;
