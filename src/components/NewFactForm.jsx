7cd46572d8f95b17fa304c2126f86ad40ec17275
ritiksharmarj <theonlyritik@gmail.com>
2023-04-03 17:26:43 +0530

    Uploading a fact to supabase

--- src/components/NewFactForm.jsx
+++ src/components/NewFactForm.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react';
 import { CATEGORIES } from '../utils/config';
+import supabase from '../supabase';
 
 const NewFactForm = ({ setShowForm, setFacts }) => {
   const [text, setText] = useState('');
   const [source, setSource] = useState('');
   const [category, setCategory] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     // 1. Prevent browser reload
     e.preventDefault();
 
     // 2. Check if data is valid. If so, create a new fact
     if (text && source && category && textLength <= 200) {
       // 3. Create a new fact object
-      const newFact = {
-        id: Math.round(Math.random() * 10000000),
-        text,
-        source,
-        category,
-        votesInteresting: 0,
-        votesMindblowing: 0,
-        votesFalse: 0,
-        createdIn: new Date().getFullYear(),
-      };
+      // const newFact = {
+      //   id: Math.round(Math.random() * 10000000),
+      //   text,
+      //   source,
+      //   category,
+      //   votesInteresting: 0,
+      //   votesMindblowing: 0,
+      //   votesFalse: 0,
+      //   createdIn: new Date().getFullYear(),
+      // };
 
       // 3. Upload fact to Supabase and receive the new fact object
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from('facts')
+        .insert([{ text, source, category }])
+        .select();
+      setIsUploading(false);
+
       // 4. Add the new fact to the UI: add the fact to state
-      setFacts((facts) => [newFact, ...facts]);
+      setFacts((facts) => [newFact[0], ...facts]);
 
       // 5. Reset input fields
       setText('');
@@ -51,6 +60,7 @@ const NewFactForm = ({ setShowForm, setFacts }) => {
           placeholder='Share a fact with the world...'
           value={text}
           onChange={(e) => setText(e.target.value)}
+          disabled={isUploading}
           className='lg:w-56 w-full sm:h-[55px] h-[38px] bg-stone-500 outline-none rounded-full px-3 py-1 sm:px-4 sm:py-3 text-lg placeholder-stone-400 border border-stone-500 focus:outline-none focus:border-stone-50 transition-all duration-300 grow'
         />
 
@@ -65,6 +75,7 @@ const NewFactForm = ({ setShowForm, setFacts }) => {
         placeholder='Trustworthy source...'
         value={source}
         onChange={(e) => setSource(e.target.value)}
+        disabled={isUploading}
         className='lg:w-56 w-full sm:h-[55px] h-[38px] bg-stone-500 outline-none rounded-full px-3 py-1 sm:px-4 sm:py-3 text-lg placeholder-stone-400 border border-stone-500 focus:outline-none focus:border-stone-50 transition-all duration-300'
       />
 
@@ -72,6 +83,7 @@ const NewFactForm = ({ setShowForm, setFacts }) => {
         required
         value={category}
         onChange={(e) => setCategory(e.target.value)}
+        disabled={isUploading}
         className='lg:w-56 w-full sm:h-[55px] h-[38px] bg-stone-500 outline-none rounded-full px-3 py-1 sm:px-4 sm:py-3 text-lg placeholder-stone-400 border border-stone-500 focus:outline-none focus:border-stone-50 transition-all duration-300'
       >
         <option value=''>Choose category</option>
@@ -82,7 +94,10 @@ const NewFactForm = ({ setShowForm, setFacts }) => {
         ))}
       </select>
 
-      <button className='btn-gradient font-coiny !leading-none uppercase text-lg sm:px-8 sm:pt-5 sm:pb-[17px] px-3 pt-2 pb-3 rounded-full transition-all duration-300 sm:hover:scale-110 hover:-rotate-2 hover:scale-105'>
+      <button
+        disabled={isUploading}
+        className='btn-gradient font-coiny !leading-none uppercase text-lg sm:px-8 sm:pt-5 sm:pb-[17px] px-3 pt-2 pb-3 rounded-full transition-all duration-300 sm:hover:scale-110 hover:-rotate-2 hover:scale-105'
+      >
         Post
       </button>
     </form>
